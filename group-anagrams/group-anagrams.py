b06c98aad1bb0bbdbf6ba5f6e8e94d7ce9ea4f7e
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-08-19 23:33:02 -0400

    made group anagrams solution cleaner

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,14 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
         anagrams = {}
         for word in strs:
             sorted_word = ''.join(sorted(word))
             if sorted_word not in anagrams:
                 anagrams[sorted_word] = []         
-            anagrams[sorted_word].append(word)
-        
-        for anagram in anagrams.values():
-            result.append(anagram)
+            anagrams[sorted_word].append(word)  
             
-        return result
+        return anagrams.values()
