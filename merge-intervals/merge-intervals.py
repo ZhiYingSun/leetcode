c1aebbec6c0440f53972c219c6737b5d611745d4
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-08-22 18:02:47 -0400

    added time and space complexity
    added time and space complexity

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -9,4 +9,4 @@ class Solution:
             else:
                 merged[-1][1] = max(merged[-1][1], interval[1])
         
-        return merged
+        return merged                                               # time: O(nlogn) space: O(n)
