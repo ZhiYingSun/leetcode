8758ec48698aa12c8f2bd0dbfe43a6f74f71c8e6
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-08-30 12:22:58 -0400

    Update kth-largest-element-in-an-array.py

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -7,8 +7,8 @@ class Solution:
         heapify(heap)   # transforms list into a heap, in-place, in linear time
         
         for num in nums:
-            heappush(heap, num)  # O(Log n)
+            heappush(heap, num)  # O(Logk)
             if len(heap) > k:
-                heappop(heap)  #  remove and return the smallest element O(Log n)
+                heappop(heap)  #  remove and return the smallest element O(Logk)
         
         return heappop(heap)
