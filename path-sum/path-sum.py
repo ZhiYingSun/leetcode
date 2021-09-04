9b4680e94ff42e6534c4255245f4cd5b14188588
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-09-04 03:25:30 -0400

    Update path-sum.py

--- path-sum/path-sum.py
+++ path-sum/path-sum.py
@@ -11,7 +11,5 @@ class Solution:
         
         if root.left is None and root.right is None and root.val == targetSum:
             return True
-    
-        targetSum -= root.val
         
-        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
