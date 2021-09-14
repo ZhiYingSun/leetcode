0969cca6c5c30ed4d9cb455331d70f8a441be691
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2021-09-13 22:13:49 -0400

    Time: 128 ms (79.15%), Space: 15.5 MB (48.07%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,10 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        seen = set()                    # use set over list or nums directly bc set look up is O(1)
+        # use sum from 0 to n to get the missing number
+        # n*(n-1)/2 - sum(of all the numbers in nums) 
         
-        for i in range(len(nums)):      # space and time complexity O(n)
-            seen.add(nums[i])
+        num_sum = 0
+        for i in range(len(nums)):
+            num_sum += nums[i]
         
-        for i in range(len(nums) + 1):
-            if i not in seen:
-                return i
+        n = len(nums) + 1                  # including n since there is a missing number
+        
+        return (n*(n-1)//2) - num_sum
